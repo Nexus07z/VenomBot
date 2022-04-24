8a361983c42dddc616059437251e8157c68d7ca3
Nexus07z <76958607+Nexus07z@users.noreply.github.com>
2022-04-24 13:17:38 -0500

    Add files via upload

---
+++ Venom.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+rosa="\033[1;35m"
+while : 
+do
+echo "${rosa}aguarde iniciando VenomBot-md..${rosa}
+
+ESTE ARQUIVO É PROGRAMADO PARA O BOT NÃO CAIRAPÓS ALGUMA QUEDA DE CONEXÃO"
+    npm start
+    sleep 1
+
+done
